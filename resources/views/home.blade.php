5b97485de5b17ffc1ddf7f923f411c55755fd5fc
BogdanBitcus <bogdan_p2007@ukr.net>
2023-11-20 16:40:40 +0200

    start 2023.11.20

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel 10.32</title>
+
+    </head>
+    <body class="antialiased">
+    Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+    </body>
+</html>
